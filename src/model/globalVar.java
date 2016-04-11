9321105caa088bc7d46d548f5b1819da791b0b91
Michiel Dhont <michiel.dhont@student.kuleuven.be>
2016-04-11 14:41:11 +0200

    'voer alle instructies uit' blijkt niet te werken

--- src/model/globalVar.java
+++ src/model/globalVar.java
@@ -17,6 +17,8 @@ public class globalVar {
 		instructies = new InstructieList();
 		try {
 			String instructieXML = "Instructions_eigenTest.xml";
+			//String instructieXML = "Instructions_30_3.xml";
+			//String instructieXML = "Instructions_20000_4.xml";
 			File file = new File(instructieXML);
 			JAXBContext jaxbContext = JAXBContext.newInstance(InstructieList.class);
 			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
